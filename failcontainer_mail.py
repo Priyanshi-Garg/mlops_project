6af8725aeee867209ce9814a62bd463633b9ffdb
Priyanshi Garg <gargpriyanshi39@gmail.com>
2020-05-26 17:04:12 +0530

    mlops

--- failcontainer_mail.py
+++ failcontainer_mail.py
@@ -1,18 +1,17 @@
-
-
 import smtplib, ssl
 
 port = 465 # FOR SSL
 smtp_server = "smtp.gmail.com"
 sender_email = "gargpriyanshi39@gmail.com"
 reciever_email = "gargpriyanshi39@gmail.com"
-password = <user_password>
+password = 'gmailpriyanshi'
 message = """\
 Subject: Mlops_project
 
-Congratulations your model has been trained successfully and you achieved accuracy more than 80%"""
+OOPs !! Some error occured in launching the container . """
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
     server.sendmail(sender_email, reciever_email, message)
+
